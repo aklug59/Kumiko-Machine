e4389797f508b21d63f9d9b2a19f3487682390e3
Aidan <aklug@luc.edu>
2024-06-23 15:31:26 -0500

    Added test object and methods so logic didn't need to be rewritten in main method.

--- src/test/java/TestMethods.java
+++ src/test/java/TestMethods.java
@@ -1,17 +1,15 @@
-package CoreLogic;
+import CoreLogic.ArduinoFacade;
+import CoreLogic.CutterHead;
 
-public class Main {
-
-    public static void main(String[] args) throws InterruptedException {
-
+public class TestMethods {
+    public void Stepper() throws InterruptedException {
         ArduinoFacade ardFac = new ArduinoFacade();
         ardFac.openConnection();
-
         CutterHead testCutter = new CutterHead(ardFac);
         testCutter.setAngle(0);
         Thread.sleep(3000);
         testCutter.setAngle(90);
-
-
     }
+
+
 }
---
+++ src/test/java/testing.java
@@ -0,0 +1,16 @@
+
+
+
+
+
+public class testing {
+    static TestMethods tester = new TestMethods();
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        tester.Stepper();
+
+    }
+}
