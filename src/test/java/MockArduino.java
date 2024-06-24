f38640d55accec6ffb363295cdc79dc59c762974
Aidan <aklug@luc.edu>
2024-06-24 11:23:10 -0500

    Added MockArduino class to make testing faster. Can now test machine functionality in java without outside connection.

---
+++ src/test/java/MockArduino.java
@@ -0,0 +1,44 @@
+public class MockArduino {
+    int stepPin = 7;
+    int dirPin = 2;
+    int steps;
+    int command;
+    int stepperCommand = 1;
+    int actuatorCommand = 2;
+    int direction = 0;
+    int numSteps;
+
+    public MockArduino (int command, int direction, int steps) {
+        this.command = command;
+        this.direction = direction;
+        this.steps = steps;
+    }
+
+    public void mockStepper() {
+
+        if (command == stepperCommand) {
+            System.out.println("Stepper command received");
+
+            //set motor direction
+            if (direction > 0) {
+                System.out.println("The motor direction is up");
+
+            } else {
+                System.out.println("The motor direction is down");
+            }
+            //Iterate over numSteps
+            for (int i = 0; i < steps; i++) {
+                numSteps++;
+            }
+            System.out.println("The number of steps is " + numSteps);
+            }
+        }
+
+    //To fill in once actuator is actually working!
+    public void mockActuator() {
+        if (command == actuatorCommand) {
+            System.out.println("Actuator command recieved");
+        }
+    }
+
+    }
--- src/test/java/testing.java
+++ src/test/java/testing.java
@@ -5,12 +5,14 @@
 
 public class testing {
     static TestMethods tester = new TestMethods();
+    static MockArduino mockArd = new MockArduino(1,1,1000);
 
 
 
     public static void main(String[] args) throws InterruptedException {
+        mockArd.mockStepper();
 
-        tester.Stepper();
+        //tester.Stepper();
 
     }
 }
