2e68900b57475f5207fdd58e8873d6cfaff3a79b
Aidan <aklug@luc.edu>
2024-06-28 20:06:43 -0500

    Forgot to add update arduino code! Now attached in Arduino Code folder.

--- Arduino Code/MergedArduinoCode.ino
+++ Arduino Code/MergedArduinoCode.ino
@@ -1,3 +1,4 @@
+//Stepper Variables
 #define stepPin 7
 #define dirPin 2
 int steps;
@@ -5,17 +6,32 @@ int command;
 int stepperCommand = 1;
 int direction = 0;
 
+
+//Actuator Variables
+int PWMPin = DAC0;
+int theDelay = 10000;
+int actuatorCommand = 2;
+int position = 0;
+
+
+
 void setup() {
+  //Actuator Pin
+  pinMode(PWMPin, OUTPUT);
+
+
+  //Stepper Pins & Serial Connection
   pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);  
   Serial.begin(9600);
+
 }
 
 void loop() {
 
   while (Serial.available() > 0) {
     command = Serial.parseInt();
-
+    
     if (command == stepperCommand) {
 
       direction = Serial.parseInt();
@@ -36,7 +52,16 @@ void loop() {
         delayMicroseconds(1000);
         digitalWrite(stepPin, LOW);
         delayMicroseconds(1000);
+
       }
+      
+    } else if (command == actuatorCommand) {
+
+      position = Serial.parseInt();
+
+      analogWrite(PWMPin, position);
+
     }
+
   }
 }
