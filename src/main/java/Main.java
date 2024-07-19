7a19120990941dd0bef2699d4f0c350172b3e06d
Aidan <aklug@luc.edu>
2024-07-19 12:52:04 -0500

    Added first artifact and executable Jar file and confirmed it works from desktop! Will create another with icon in future.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+
+import static View.GUI.populateGUI;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+
+        populateGUI();
+
+
+    }
+}
